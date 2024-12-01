c210533e61c095fa512c9e5b0f3ac15650b4b39e
akaimetsu <103828900+akaimetsu@users.noreply.github.com>
2024-12-01 14:02:06 +0300

    Add files via upload

---
+++ test.js
@@ -0,0 +1,35 @@
+// Yorum satırı
+document.getElementById("btn").addEventListener("click", function () {
+    //alert("Emre Kırın annesine selam olsun!!!");
+    window.location.href = 'samuel.html';
+});
+
+document.getElementById("button2").addEventListener("click", function () {
+    window.location.href = 'kil.html';
+});
+
+document.getElementById("button3").addEventListener("click", function () {
+    window.location.href = 'arfas.html';
+});
+
+document.getElementById("button4").addEventListener("click", function () {
+    window.location.href = 'tosya.html';
+});
+
+
+
+
+// Video elementini seçiyoruz
+// const videoElement = document.getElementById('video');
+
+// Video bittiğinde çalışacak olay
+// videoElement.addEventListener('ended', function () {
+  // Yeni bir video kaynağı belirtiyoruz
+ //  const newSource = 'yeni_video.mp4'; // Yeni video dosyanızın yolu
+  
+  // Video kaynağını değiştir
+ //  videoElement.src = newSource;
+  
+  // Yeni videoyu başlat
+ //  videoElement.play();
+ // });
